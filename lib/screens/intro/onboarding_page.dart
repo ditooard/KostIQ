d839fca19eaefca765c16b9ad2629cb69dfcba10
Dito Ardi Pratama <dito010504@gmail.com>
2025-07-30 19:06:49 +0700

    push

--- lib/screens/intro/onboarding_page.dart
+++ lib/screens/intro/onboarding_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/theme_provider.dart';
 
-class OnboardingPage extends StatefulWidget {
-  const OnboardingPage({super.key});
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key, required this.title});
+  final String title;
 
-  @override
-  State<OnboardingPage> createState() => _OnboardingPageState();
-}
-
-class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDarkMode = themeProvider.isDarkMode;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        actions: [
+          Switch(
+            value: isDarkMode,
+            onChanged: (val) => themeProvider.toggleTheme(val),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to KostIQ',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+    );
   }
 }
--- lib/screens/intro/splash_page.dart
+++ lib/screens/intro/splash_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:manajemen_kost/screens/intro/onboarding_page.dart';
+import 'package:manajemen_kost/utils/navigation_helper.dart';
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      NavigationHelper.push(
+          context, const OnboardingPage(title: 'Flutter Demo Home Page'));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            FlutterLogo(size: 80),
+            SizedBox(height: 20),
+            Text("Welcome to KostIQ", style: TextStyle(fontSize: 20))
+          ],
+        ),
+      ),
+    );
   }
 }
